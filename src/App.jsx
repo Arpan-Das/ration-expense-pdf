4683f096a7a6bdf718b148e8e0740f2c0a2203cc
Arpan <arpandas200015@outlook.com>
2025-04-06 02:32:13 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PDF from './pdf/pdf'
+
+function App() {
+
+  return (
+    <>
+      <PDF/>
+    </>
+  )
+}
+
+export default App
